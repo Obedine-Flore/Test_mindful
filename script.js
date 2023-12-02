d442c6d1602731bb763dfc91e3a00a40e59bc490
Obedine-Flore <f.obedine@alustudent.com>
2023-12-02 18:14:01 +0100

    my commit

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
 
+/* Single item*/
 if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active');
@@ -13,3 +19,20 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+
+/* Login */
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
